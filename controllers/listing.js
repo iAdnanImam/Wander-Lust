fc3c4d3be17752e04415229546fc8c56a4890a73
Adnan Imam <adnanimam972@gmail.com>
2025-06-20 00:37:23 +0530

    Added Project Files

--- controllers/listing.js
+++ controllers/listing.js
@@ -67,13 +67,17 @@ module.exports.edit=async (req, res) => {
 
 module.exports.update=async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.findByIdAndUpdate(id, req.body.listing);
+    let listing = await Listing.findByIdAndUpdate(id, req.body.listing, {
+      new: true,
+      runValidators: true
+    });
     if(typeof req.file !=="undefined"){
       let url=req.file.path;
       let filename=req.file.filename;
       listing.image={url,filename};
       await listing.save();
     }
+    console.log(req.body);
     req.flash("success","Listing Updated!!");
     res.redirect(`/listings/${id}`);
 };
